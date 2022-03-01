25de4dacaa3aa5f157a5a7c52f3d1cdf3e38d052
Megan Hong <mhong051@uottawa.ca>
2022-02-28 22:03:38 -0500

    rotate matrix fix

--- ArrayStringQuestions/Megan/RotateMatrix.js
+++ ArrayStringQuestions/Megan/RotateMatrix.js
@@ -1,13 +1,49 @@
-// [1,2,3]     [1,4,7]
-// [4,5,6] --> [2,5,8]
-// [7,8,9]     [3,6,9]
-
-const RotateMatrix = (nXnMatrix) => {
-  for (i = 0; i < nXnMatrix.length; i++) {
-    for (j = i + 1; j < nXnMatrix[i].length; j++) {
-      const temp = nXnMatrix[i][j];
-      nXnMatrix[i][j] = nXnMatrix[j][i];
+const RotateMatrixInplace = (nXnMatrix) => {
+  if (nXnMatrix.length <=1){
+    return nXnMatrix
+  }
+	const length = nXnMatrix.length;
+  
+  for (i = 0; i<length; i++){
+  	for(j = i+1; j<length; j++){
+    	const temp = nXnMatrix[i][j]
+    	nXnMatrix[i][j]= nXnMatrix[j][i];
       nXnMatrix[j][i] = temp;
     }
   }
-};
+  
+  for(i = 0; i<length; i++){
+  	let l = 0;
+    let r = length -1;
+    
+    while(l<r){
+    	const temp = nXnMatrix[i][l];
+      nXnMatrix[i][l]=nXnMatrix[i][r];
+      nXnMatrix[i][r] = temp;
+      r--;
+      l++;
+    }
+  }
+  
+  return nXnMatrix;
+}
+
+const RotateMatrixExtraMem = (nXnMatrix) => {
+  if (nXnMatrix.length <=1){
+    return nXnMatrix
+  }
+	const length = nXnMatrix.length;
+  const rotatedArray = new Array(length);
+  for(i = 0; i< length; i++){
+  	rotatedArray[i] = new Array(length);
+  }
+  
+  for(i = 0; i<length; i++){
+ 		for(j = 0; j<length; j++){
+    	rotatedArray[i][j] = nXnMatrix[length-1-j][i]
+    }
+  }
+    
+  return rotatedArray;
+}
+
